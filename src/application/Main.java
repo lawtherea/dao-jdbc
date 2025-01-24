78f33eb370ce51e2ea70006f3105143327dc27a7
lawtherea <leilamariabiggi@outlook.com>
2025-01-24 18:40:43 -0300

    delete implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,9 +7,11 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -48,5 +50,15 @@ public class Main {
         seller.setName("Martha Wayne");
         sellerDao.update(seller);
         System.out.println("Updated completed");
+
+        System.out.println();
+
+        System.out.println("=== TEST 6: seller delete ===");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
