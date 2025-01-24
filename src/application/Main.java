e983b55fc3c371ecbce26c844be25e3e22a1e76a
lawtherea <leilamariabiggi@outlook.com>
2025-01-23 21:22:03 -0300

    SellerDaoJDBC and DaoFactory added

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,7 +10,11 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
+
         Seller seller = new Seller(21, "John Doe", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
