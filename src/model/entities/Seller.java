e556fb1ec6f3f608974031517fd07f63363bb075
lawtherea <leilamariabiggi@outlook.com>
2025-01-23 21:53:28 -0300

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -9,7 +9,7 @@ public class Seller implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer id;
-    private String nome;
+    private String name;
     private String email;
     private Date birthDate;
     private Double baseSalary;
@@ -18,9 +18,9 @@ public class Seller implements Serializable {
 
     public Seller() {}
 
-    public Seller(Integer id, String nome, String email, Date birthDate, Double baseSalary, Department department) {
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
         this.id = id;
-        this.nome = nome;
+        this.name = name;
         this.email = email;
         this.birthDate = birthDate;
         this.baseSalary = baseSalary;
@@ -35,12 +35,12 @@ public class Seller implements Serializable {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -90,6 +90,6 @@ public class Seller implements Serializable {
 
     @Override
     public String toString() {
-        return "Seller{" + "id=" + id + ", nome='" + nome + '\'' + ", email='" + email + '\'' + ", birthDate=" + birthDate + ", baseSalary=" + baseSalary + ", department=" + department + '}';
+        return "Seller{" + "id=" + id + ", name='" + name + '\'' + ", email='" + email + '\'' + ", birthDate=" + birthDate + ", baseSalary=" + baseSalary + ", department=" + department + '}';
     }
 }
