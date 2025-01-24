f2453c8c8db6ede401b47899b127c4af9812d941
lawtherea <leilamariabiggi@outlook.com>
2025-01-24 18:34:51 -0300

    update implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -40,5 +40,13 @@ public class Main {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println();
+
+        System.out.println("=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Updated completed");
     }
 }
